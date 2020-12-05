eac9c234f0f6a062f170ad3e8de7f391119f49df
Emmanuel Nicolet <emmanuel.nicolet@gmail.com>
2020-12-05 11:25:51 +0100

    translate to binary string

--- day05/day05.py
+++ day05/day05.py
@@ -2,24 +2,7 @@
 
 import sys
 
-def decode(s, hi, lc):
-  lo = 0
-  for c in s:
-    mid = (lo + hi) // 2
-    if c == lc:
-      hi = mid - 1
-    else:
-      lo = mid + 1
-  return lo
-
-def decode_row(bp):
-  return decode(bp[:7], 127, 'F')
-
-def decode_column(bp):
-  return decode(bp[-3:], 7, 'L')
-
-
-seat_ids = [decode_row(bp.strip()) * 8 + decode_column(bp.strip()) for bp in sys.stdin]
+seat_ids = [int(bp.strip().translate(str.maketrans('FBLR', '0101')), 2) for bp in sys.stdin]
 
 max_seat_id = max(seat_ids)
 print('part 1:', max_seat_id)
