4454737341edafa08c59ea056287a5d0480acc97
Emmanuel Nicolet <emmanuel.nicolet@gmail.com>
2020-12-06 10:59:18 +0100

    day06

---
+++ day06/day06.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import Counter
+
+group_answers = [answers.strip().split("\n") for answers in sys.stdin.read().split("\n\n")]
+print('part 1:', sum(len(set(''.join(answers))) for answers in group_answers))
+print('part 2:', sum(v == len(answers) for answers in group_answers for _, n in Counter(''.join(answers)).items()))
